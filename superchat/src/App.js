64181844a95da9d9881bc000e3cca2663099bd27
jay <jayww3@outlook.com>
2021-11-02 01:20:15 -0400

    added some code

---
+++ superchat/src/App.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import './App.css';
+
+import firebase from "firebase/app";
+import firestore from "firestore"
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectiondata } from 'react-firebase-hooks/firestore';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
